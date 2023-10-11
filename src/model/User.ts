66ffa72a326cf6a7be6915383fdf1cc654bc7df7
nitishk@instaacoders.com <nitishk@instaacoders.com>
2023-10-11 22:37:10 +0530

    added more field in users model

--- src/model/User.ts
+++ src/model/User.ts
@@ -2,8 +2,15 @@ import {Schema,Document,model} from 'mongoose'
 
 export interface IUser extends Document{
     name : string,
+    first_name : string,
+    last_name : string,
+    gender : string,
     email : string,
     mobile : string,
+    dob : Date,
+    position : string,
+    nationality : string,
+    program_name : string, 
     password : string,
     verifyToken : string,
     isUserVerified : boolean
@@ -14,12 +21,41 @@ const UserSchema:Schema = new Schema({
         type :String,
         default : null
     },
+    first_name : {
+        type :String,
+        default : null
+    },
+    last_name : {
+        type :String,
+        default : null
+    },
+    gender : {
+        type: String,
+        default: null
+    },
+
     email :{
         type : String,
         unique : true
     },
     mobile : {
         type  : String,
+        default : null
+    },
+    dob : {
+        type : Date,
+        default : null
+    },
+    position : {
+        type : String,
+        default : null
+    },
+    nationality : {
+        type : String,
+        default : null
+    },
+    program_name : {
+        type : String,
         default : null
     },
     password : {
